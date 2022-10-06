1d98d70fd00e457fa7725e4a05db2871add46d2c
unknown <sie07422@jmail.ac.kr>
2022-10-06 12:55:42 +0900

    first

---
+++ components/Navbar.js
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css";
+export default function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.links}>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <Link href="/profile">
+          <a>Profile</a>
+        </Link>
+        <Link href="/coins">
+          <a>Coins</a>
+        </Link>
+        <Link href="/profile/contact">
+          <a>연락</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
