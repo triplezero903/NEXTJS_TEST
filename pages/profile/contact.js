c15b5a96f7ee841af3a9247ec9fad54bd24361b4
triplezero903 <triplezero903@hotmail.com>
2022-10-19 20:44:51 +0900

    last2

--- pages/profile/contact.js
+++ pages/profile/contact.js
@@ -4,7 +4,7 @@ import styles from "../../styles/Home.module.css";
 export default function Contact() {
   return (
     <div className="styles.main">
-      <h1>"저랑 연락하고 싶어요!"</h1>
+      <h1>&quot;저랑 연락하고 싶어요!&quot;</h1>
       <a>
         아쉽지만 제 번호랑 인스타그램 아이디는 쉽게 밝힐 수 있는 게 아니에요.
         많은 양해 부탁드려요 ㅠ
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -4,7 +4,7 @@ export default class index extends PureComponent {
   render() {
     return (
       <div>
-        <h1>"저는 누굴까요?"</h1>
+        <h1>&quot;저는 누굴까요?&quot;</h1>
         <a>
           안녕하세요! 저는 중부대학교 고양캠퍼스 정보보호학과 2학년 XXX입니다.
         </a>
