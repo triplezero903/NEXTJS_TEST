c57c1cc8b6f22deacbb4a418d66c02674c3a1234
triplezero903 <triplezero903@hotmail.com>
2022-10-19 20:36:08 +0900

    last

---
+++ components/Navbar.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css";
+export default function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.links}>
+        <Link href="/">
+          <a>홈 </a>
+        </Link>
+        <Link href="/about">
+          <a>사이트에 대해서</a>
+        </Link>
+        <Link href="/profile">
+          <a>본인 소개</a>
+        </Link>
+        <Link href="/profile/contact">
+          <a>연락</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ pages/profile/index.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from "react";
+
+export default class index extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>"저는 누굴까요?"</h1>
+        <a>
+          안녕하세요! 저는 중부대학교 고양캠퍼스 정보보호학과 2학년 XXX입니다.
+        </a>
+      </div>
+    );
+  }
+}
